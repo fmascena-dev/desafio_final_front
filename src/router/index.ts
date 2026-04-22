b1d55d27f4cae5aa5b7417180a336c362faa4422
fmascena-dev <felipe.mascena.dev@gmail.com>
2026-04-22 01:28:38 -0300

    chore: implementation the adjusted of project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  scrollBehavior: () => ({ top: 0 }),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/abrigos',
+      name: 'shelters',
+      component: () => import('@/views/SheltersView.vue'),
+    },
+    {
+      path: '/abrigos/:id',
+      name: 'shelter-detail',
+      component: () => import('@/views/ShelterDetailView.vue'),
+    },
+    {
+      path: '/cadastrar',
+      name: 'register-shelter',
+      component: () => import('@/views/RegisterShelterView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('@/views/NotFoundView.vue'),
+    },
+  ],
 })
 
 export default router
